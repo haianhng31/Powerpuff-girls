ccec48502789266d76d6d05ab782c9d22a42f323
Alondra Franco <alondralezfra@hotmail.com>
2025-07-17 21:57:48 -0500

    Installed Tailwind and Restructured NavBar using Routes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar.js';
+import HealthTab from './Components/HealthTab/HealthTab.js';
+import TravelingTab from './Components/TravelTab/TravelTab.js';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Router>
         <Navbar />
+          <Routes>
+            <Route path="/HealthTab" element={<HealthTab />} />
+            <Route path="/TravelTab" element={<TravelingTab />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import './Navbar.css'
+import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-    <nav className='navbar'>
-        <div className='navbar-left'>
-            Logo will go here for home navigation
+    <nav className="bg-white shadow-md px-6 py-4 flex items-center justify-between">
+        <div className="text-2xl font-bold text-gray-800">
+            <Link to='/'>Home</Link>
         </div>
-        <div className='navbar-right'>
-            <ul className='navbar-ul'>
-                <li className='navbar-li'>
-                    <a href='../HealthTab/HealthTab.js'>Health</a>
-                </li>
-                <li className='navbar-li'>
-                    <a href='../TravelTab/TravelTab.js'>Traveling</a>
-                </li>
-            </ul>
+        <div className="flex gap-6 text-lg text-gray-700">
+            <Link to='/HealthTab' className="hover:text-green-600 transition duration-200">Health</Link>
+            <Link to='/TravelTab'className="hover:text-green-600 transition duration-200">Travel</Link>
         </div>
     </nav>
     );
