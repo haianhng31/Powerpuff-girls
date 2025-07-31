501330696bd7900a3a91223fb149c83205022910
haianhng31 <anguyen137@fordham.edu>
2025-07-30 20:07:22 -0700

    Create article schema & routes

---
+++ backend/models/Articles.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const articleSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["book", "product", "app", "place"],
+    required: true,
+  },
+  category: { type: String, required: true },
+  title: { type: String, required: true },
+  ranking: { type: Number, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  creatorName: { type: String, required: true },
+  date: { type: Date, required: true },
+  imageUrl: { type: String, required: true },
+  link: { type: String, default: null },
+  locationId: { type: String, default: null },
+});
+
+const Article = mongoose.model("Article", articleSchema);
+export default Article;
