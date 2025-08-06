63697ea05cba1f2533269d780f306fe2eb2a5470
Alondra Franco <alondralezfra@hotmail.com>
2025-08-05 22:29:40 -0500

    Changed backend to port 5000, made imageUrl optional in article schema, fixed connection from react to backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 
 // connect to MongoDB
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
