9bfb085f86b7e352f9e56f00edda001d02374ead
Alondra Franco <alondralezfra@hotmail.com>
2025-08-12 15:30:45 -0500

    Added Home Link in Nav Bar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -6,11 +6,12 @@ const Navbar = () => {
     return (
     <nav className="bg-white shadow-md px-6 h-16 flex items-center justify-between">
         <div className="text-xl text-gray-800">
-            <Link to='/Home'>
+            <Link to='/'>
                 <img src={powerpuffLogo} alt="Powerpuff Logo" className="h-20 w-auto"></img>
             </Link>
         </div>
         <div className="flex gap-6 text-md text-gray-600">
+            <Link to='/Home' className="hover:text-pink-600 transition duration-200">Home</Link>
             <Link to='/HealthTab' className="hover:text-pink-600 transition duration-200">Health</Link>
             <Link to='/TravelTab'className="hover:text-pink-600 transition duration-200">Travel</Link>
         </div>
